8d7853b56f5c48d504c7aca50ae7d16039a452dc
bodde <bodde>
2026-09-20 09:54:06 +0200

    JsonExtensions renamed to ToJsonExtensions; skipDefaultValues argument added to ToFormattedJson

--- Bodde.Json.Extensions.Test/ToJsonExtensions_ToFormattedJson.cs
+++ Bodde.Json.Extensions.Test/ToJsonExtensions_ToFormattedJson.cs
@@ -1,10 +1,11 @@
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using Bodde.Json.Extensions.Test.Helpers;
 using Bodde.Json.Extensions.Test.Models;
 
 namespace Bodde.Json.Extensions.Test;
 
-public class JsonExtensions_ToFormattedJson
+public class ToJsonExtensions_ToFormattedJson
 {
     [Fact]
     public void Null_Throws_ArgumentNullException()
@@ -25,7 +26,6 @@ public class JsonExtensions_ToFormattedJson
             "  \"FirstName\": \"John\",",
             "  \"LastName\": \"Doe\",",
             "  \"BirthDate\": \"1980-01-01T00:00:00\",",
-            "  \"Role\": \"Developer\",",
             "  \"Department\": {",
             "    \"Id\": 1,",
             "    \"Name\": \"Engineering\"",
@@ -50,13 +50,11 @@ public class JsonExtensions_ToFormattedJson
             "\"FirstName\":\"John\",",
             "\"LastName\":\"Doe\",",
             "\"BirthDate\":\"1980-01-01T00:00:00\",",
-            "\"Role\":\"Developer\",",
             "\"Department\":{",
             "\"Id\":1,",
             "\"Name\":\"Engineering\"",
             "}",
             "}");
-
         var actual = sut.ToFormattedJson(indented: false);
 
         Assert.NotEmpty(actual);
@@ -64,7 +62,53 @@ public class JsonExtensions_ToFormattedJson
     }
 
     [Fact]
-    public void EnumsAsStrings_False_Returns_Indented_Json()
+    public void NonNull_Department_Null_Role_Default_Returns_Json_Without_DepartmentRole()
+    {
+        Employee sut = ModelBuilder.CreateJohnDoe();
+        sut.Department = null;
+        sut.Role = default;
+        
+        var expected = string.Join(Environment.NewLine, [
+            "{",
+            "  \"Id\": 1,",
+            "  \"FirstName\": \"John\",",
+            "  \"LastName\": \"Doe\",",
+            "  \"BirthDate\": \"1980-01-01T00:00:00\"",
+            "}"
+        ]);
+
+        var actual = sut.ToFormattedJson();
+
+        Assert.NotEmpty(actual);
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void NonNull_Department_Null_Id_And_Role_Default_SkipDefaultValues_False_Returns_Json_With_Department_Role()
+    {
+        Employee sut = ModelBuilder.CreateJohnDoe();
+        sut.Department = null;
+        sut.Role = default;
+        
+        var expected = string.Join(Environment.NewLine, [
+            "{",
+            "  \"Id\": 1,",
+            "  \"FirstName\": \"John\",",
+            "  \"LastName\": \"Doe\",",
+            "  \"BirthDate\": \"1980-01-01T00:00:00\",",
+            "  \"Role\": \"Developer\",",
+            "  \"Department\": null",
+            "}"
+        ]);
+
+        var actual = sut.ToFormattedJson(skipDefaultValues: false);
+
+        Assert.NotEmpty(actual);
+        Assert.Equal(expected, actual);
+    } 
+
+    [Fact]
+    public void EnumsAsStrings_False_SkipDefaultValues_False_Returns_Indented_Json()
     {
         Employee sut = ModelBuilder.CreateJohnDoe();
         
@@ -82,14 +126,14 @@ public class JsonExtensions_ToFormattedJson
             "}"
         ]);
 
-        var actual = sut.ToFormattedJson(enumsAsStrings: false);
+        var actual = sut.ToFormattedJson(enumsAsStrings: false, skipDefaultValues: false);
 
         Assert.NotEmpty(actual);
         Assert.Equal(expected, actual);
     }
 
     [Fact]
-    public void NonNull_Indented_False_EnumsAsStrings_False_Returns_NotIndented_EnumsAsValues_Json()
+    public void NonNull_Indented_False_EnumsAsStrings_False_SkipDefaultValues_False_Returns_NotIndented_EnumsAsValues_Json()
     {
         Employee sut = ModelBuilder.CreateJohnDoe();
         
@@ -106,9 +150,10 @@ public class JsonExtensions_ToFormattedJson
             "}",
             "}");
 
-        var actual = sut.ToFormattedJson(indented: false, enumsAsStrings: false);
+        var actual = sut.ToFormattedJson(indented: false, enumsAsStrings: false, skipDefaultValues: false);
 
         Assert.NotEmpty(actual);
         Assert.Equal(expected, actual);
     }
+
 }
